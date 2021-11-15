5eb786423c79fa12e878f3ae5f48b5e9fe35916b
unknown <ivanchinocastro773@gmail.com>
2021-11-15 13:29:13 -0300

    [updated/files]: ordene codigo

--- ejer2/ejer2.js
+++ ejer2/ejer2.js
@@ -1,18 +1,19 @@
-const boton = document.querySelector('.button')
-const input = document.querySelector('.input')
+const btn = document.querySelector('.button')
+const btn2 = document.querySelector('.button2')
 const content= document.querySelector('.content') //clase
-boton.style.background ="lightblue";
-boton.style.borderRadius ="10px";
+btn.style.background ="lightblue";
+btn.style.borderRadius ="10px";
+
 const miH1 = document.getElementById('h1');
 const myInput = document.getElementById('myinput');
-//const btn = document.getElementById('btn').addEventListener("click",changeData());
-
+//Texto por default
 miH1.innerText='Texto';
-
+//Funciones
 const changeData =()=>miH1.innerText=myInput.value;
-
 const changeWithStyle=()=>{
     content.style.color = "black";
     content.style.background = "red";
    return miH1.innerText=myInput.value; 
 }
+btn.addEventListener('click',changeData);
+btn2.addEventListener('click',changeWithStyle);
